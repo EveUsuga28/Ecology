ba3946029b335f74745f53fc5031acf1d176247a
JulianDavidOsorioVillegas <jdosorio9236@misena.edu.co>
2021-05-27 13:45:44 -0500

    haga pull

--- resources/views/grupo/create.blade.php
+++ resources/views/grupo/create.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+
+@endsection
+
 @section('content')
 
 @if(Session::has('mensaje'))
@@ -7,33 +11,6 @@
 
 @endif
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-
-        .boton{
-            width: 54%;
-        }
-
-    </style>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.css"/>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.js"></script>
-
-
-</head>
-<body>
-
-
-<br>
-<br>
 <div class="container mt-4">
     <div class="card border-info" >
     <div class="card-header bg-success text-white" >
@@ -43,35 +20,42 @@
 <form class="container" action="{{ url('grupo') }} " method="post" enctype="multipart/form-data">
 @csrf
 
-<div class="form-group">
-<label for="Grupo">Grupo</label><br>
-<input type="text" name="Grupo" id="Grupo" required maxLength="4"><br>
-</div>
+    <div class="form-group">
+        <label for="Grupo">Grupo</label>
+        <br>
+        <input type="text" name="Grupo" id="Grupo" required maxLength="4">
+        <br>
+    </div>
 
-<div class="form-group">
-<label for="id">institución</label><br>
+    <div class="form-group">
+        <label for="id_institucion">institución</label>
+        <br>
+        
+        <select name="id_institucion" id="id_institucion">
+            <option value="">Selecciona una opcion</option>
+            @foreach ($datos as $grupos)
+                <option value="{{ $grupos->id}}">{{ $grupos->nombre}}</option>
+            @endforeach
+        </select>
+        
+        <br>
+    </div>
 
-<select name="id" id="id">
-    @foreach ( $institucion as $grupos)
-        <option value="{{ $grupos->id}}">{{ $grupos->Nombre}}</option>
-    @endforeach
-</select><br>
-</div>
+    <div class="form-group">
+        <label for="Estado">Estado</label>
+        <br>
 
-<div class="form-group">
-<label for="Estado">Estado</label><br>
+        <select name="Estado" id="Estado">
+            <option value="1">habilitado</option>
+            <option value="0">deshabilitado</option>
+        </select>
+        <br>
+    </div>
 
-<select name="Estado" id="Estado">
-        <option value="1">habilitado</option>
-        <option value="0">deshabilitado</option>
-</select><br>
-</div>
-
-<input class="btn btn-success" type="submit" value="Enviar"><br><br>
+    <input class="btn btn-success" type="submit" value="Enviar"><br><br>
 
 </form>
 
-</div>
-</div>
-</body>
+@endsection
+@section('js')
 @endsection
--- resources/views/grupo/edit.blade.php
+++ resources/views/grupo/edit.blade.php
@@ -1,35 +1,11 @@
 @extends('layouts.app')
+@section('css')
+
+@endsection
 
 @section('content')
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-
-        .boton{
-            width: 54%;
-        }
-
-    </style>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.css"/>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.js"></script>
-
-
-</head>
-<body>
-
-
-<br>
-<br>
 <div class="container mt-4">
     <div class="card border-info" >
     <div class="card-header bg-success text-white" >
@@ -43,7 +19,7 @@
 
 <div class="form-group">
 <label for="Grupo">Grupo</label><br>
-<input type="text" name="Grupo" id="Grupo" value="{{$grupo->Grupo}}" required maxLength="4"><br>
+<input type="text" name="Grupo" id="Grupo" value="{{$grupo->grupo}}" required maxLength="4"><br>
 </div>
 <div class="form-group">
 <label for="id">Institución</label><br>
@@ -60,7 +36,6 @@
 
 </form>
 
-</div>
-</div>
-</body>
+@endsection
+@section('js')
 @endsection
