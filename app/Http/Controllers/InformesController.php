8c11ef7d3c4396eeeb6074d228180be1b9a55e46
EveUsuga28 <eveusugafer@gmail.com>
2021-05-31 00:55:54 -0500

    Informes

--- app/Http/Controllers/InformesController.php
+++ app/Http/Controllers/InformesController.php
@@ -11,13 +11,33 @@ class InformesController extends Controller
 {
 
      public function all(Request $request){
-       $reciclaje_grupos = DB::table('institucions')
-        ->select('institucions.*')
-        ->orderBy('id','DESC')
-        ->get();
-        return response(json_encode($reciclaje_grupos),200)->header('Content-type','text/plain');
+        $sql=DB::table('reciclaje_grupos')
+   ->Join('grupos', 'reciclaje_grupos.id_grupo', '=', 'grupos.id')
+   ->Join('institucions','grupos.id_institucion', '=','institucions.id')
+   ->select('reciclaje_grupos.*',
+   'grupos.*','institucions.*')
+   ->orderBy('total_puntaje_grupo','DESC')
+   ->get();
+   return response(json_encode($sql),200)->header('Content-type','text/plain');
+      // $reciclaje_grupos = DB::table('reciclaje_grupos')
+        //->select('reciclaje_grupos.*')
+
+        //->get();
+        //return response(json_encode($reciclaje_grupos),200)->header('Content-type','text/plain');
         //return "hola";
+        //public function all(Request $request){
+          //  $reciclaje_grupos = DB::table('reciclaje_grupos ')
+            //->join("reciclaje_grupos","id_grupo", "=", "grupos.id")
+             //->select("reciclaje_grupos.total_puntaje_grupo"
+             //,"grupos.grupo")
+             //->get();
+             //SELECT A.total_puntaje_grupo,B.grupo
+             //from reciclaje_grupos A join grupos B on(A.id_grupo = B.id)
+             //return response(json_encode($reciclaje_grupos),200)->header('Content-type','text/plain');
+             //return "hola";
 }
+
+
 
 
     public function index(){
